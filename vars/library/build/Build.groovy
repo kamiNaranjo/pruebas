e58adcfb01975c9c6e9af98f7cebe61f2e23b5f1
marinamd <mcnaranjo@sura.com.co>
2018-04-02 14:54:23 -0500

    prueba 3

--- vars/library/build/Build.groovy
+++ vars/library/build/Build.groovy
@@ -1,9 +1,21 @@
 package library.build
 
-def build(url) {
-	git credentialsId: '0df8e168-c5ab-452f-b993-29496cfbcd29', url: '${url}'
-	gradle 'Gradle 2.3'
-	sh 'gradle clean build'
+def call(url) {
+	def config = [:]
+	
+	pipeline {
+		agent any
+		
+		stages {
+			stage('Build') {
+				steps{
+					git credentialsId: '0df8e168-c5ab-452f-b993-29496cfbcd29', url: '${config.url}'
+					gradle 'Gradle 2.3'
+					sh 'gradle clean build'
+				}
+			}
+		}
+	}
 }
 
 return this
