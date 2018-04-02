a76223f89154abba28fb20ce8e3657f5e97fc69e
marinamd <mcnaranjo@sura.com.co>
2018-04-02 15:41:59 -0500

    prueba 5

--- vars/library/build/Build.groovy
+++ vars/library/build/Build.groovy
@@ -1,7 +1,10 @@
 package library.build
 
-def call(url) {
+def call(body) {
 	def config = [:]
+	body.resolveStrategy = Closure.DELEGATE_FIRST
+	body.delegate = config
+	body()
 	
 	pipeline {
 		agent any
