a1c902fcd6555182ea003fbd3a328cae027623bd
marinamd <mcnaranjo@sura.com.co>
2018-04-02 15:48:30 -0500

    prueba 6

--- vars/library/build/Build.groovy
+++ vars/library/build/Build.groovy
@@ -12,9 +12,10 @@ def call(body) {
 		stages {
 			stage('Build') {
 				steps{
-					git credentialsId: '0df8e168-c5ab-452f-b993-29496cfbcd29', url: '${config.url}'
+					echo ${config.url}
+					/*git credentialsId: '0df8e168-c5ab-452f-b993-29496cfbcd29', url: '${config.url}'
 					gradle 'Gradle 2.3'
-					sh 'gradle clean build'
+					sh 'gradle clean build'*/
 				}
 			}
 		}
