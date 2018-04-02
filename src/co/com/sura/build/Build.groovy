6c3999afb9d1f24d643cbb6384c6e6ad819c937b
marinamd <mcnaranjo@sura.com.co>
2018-04-02 14:14:44 -0500

    prueba 1

---
+++ src/co/com/sura/build/Build.groovy
@@ -0,0 +1,9 @@
+package co.com.sura.build
+
+def build(url) {
+	git credentialsId: '0df8e168-c5ab-452f-b993-29496cfbcd29', url: '${url}'
+	gradle 'Gradle 2.3'
+	sh 'gradle clean build'
+}
+
+return this
---
+++ vars/library/build/Build.groovy
@@ -0,0 +1,5 @@
+package library.build
+
+class Build {
+
+}
