2af0ae319db89abb4c6f435d716c704f4138ef1b
marinamd <mcnaranjo@sura.com.co>
2018-04-02 16:13:04 -0500

    prueba 8

--- vars/library/build/suraShared.groovy
+++ vars/library/build/suraShared.groovy
@@ -13,6 +13,7 @@ def call(body) {
 			stage('Build') {
 				steps{
 					echo ${config.url}
+
 					/*git credentialsId: '0df8e168-c5ab-452f-b993-29496cfbcd29', url: '${config.url}'
 					gradle 'Gradle 2.3'
 					sh 'gradle clean build'*/
