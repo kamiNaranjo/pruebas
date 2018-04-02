5f15b4b5f702e1e000d7369d22f2907a16b672dc
marinamd <mcnaranjo@sura.com.co>
2018-04-02 14:35:54 -0500

    prueba 1

--- vars/library/build/Build.groovy
+++ vars/library/build/Build.groovy
@@ -1,5 +1,9 @@
 package library.build
 
-class Build {
-
+def build(url) {
+	git credentialsId: '0df8e168-c5ab-452f-b993-29496cfbcd29', url: '${url}'
+	gradle 'Gradle 2.3'
+	sh 'gradle clean build'
 }
+
+return this
