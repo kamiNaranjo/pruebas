412b4810f13be5d4ae6e380bb446b6a46b62166e
marinamd <marinamd@PSEG5BR0CG2.suramericana.com.co>
2018-04-03 09:04:28 -0500

    prueba src

---
+++ src/co/com/sura/dsl/buildSura.groovy
@@ -0,0 +1,12 @@
+package co.com.sura.dsl
+
+def build(url) {
+	cleanWs()
+	git credentialsId: '0df8e168-c5ab-452f-b993-29496cfbcd29', url: "${url}"
+	sh 'gradle clean build'	
+}
+
+def publishJunitTest(path) {
+	junit "${path}"
+}
+
