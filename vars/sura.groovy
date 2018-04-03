34169b5481b033969ad03bc82e8f8050a282e796
kamiNaranjo <cami.tsuki@gmail.com>
2018-04-03 08:24:55 -0500

    Update sura.groovy

--- vars/sura.groovy
+++ vars/sura.groovy
@@ -6,6 +6,8 @@ def call(url) {
           steps {
             cleanWs()
             git credentialsId: '0df8e168-c5ab-452f-b993-29496cfbcd29', url: "${url}"
+            gradle 'Gradle 2.3' 
+            sh 'gradle clean build'
           }
         }
       }
