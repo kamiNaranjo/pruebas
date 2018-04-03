2f4e93b902e601f2817fbe7af9965d2b08b60cc8
kamiNaranjo <cami.tsuki@gmail.com>
2018-04-03 08:28:37 -0500

    Update sura.groovy

--- vars/sura.groovy
+++ vars/sura.groovy
@@ -3,8 +3,7 @@ def call(url) {
     pipeline {
       agent any
       stages {
-        stage('Build') {
-          stage('GIT') {
+       stage('GIT') {
             steps {
                 git credentialsId: '0df8e168-c5ab-452f-b993-29496cfbcd29', url: "${url}"
 
