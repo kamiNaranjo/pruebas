a7db81581864aaa4f65e770f5077e8a990941f88
kamiNaranjo <cami.tsuki@gmail.com>
2018-04-03 08:38:55 -0500

    Update sura.groovy

--- vars/sura.groovy
+++ vars/sura.groovy
@@ -7,7 +7,7 @@ def call(url) {
         stages {
             stage('GIT') {
                 steps {
-                    git credentialsId: '0df8e168-c5ab-452f-b993-29496cfbcd29', url:'http://inboggit01.suramericana.com.co:8080/gitbucket/git/Devops/DevopsApi.git'
+                    git credentialsId: '0df8e168-c5ab-452f-b993-29496cfbcd29', url:"${url}"
                 }
             }
             stage('Gradle') {
