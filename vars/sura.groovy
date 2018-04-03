3aaddef5c8bdd349915ec0e6639a567eaf0eb084
kamiNaranjo <cami.tsuki@gmail.com>
2018-04-03 08:37:52 -0500

    Update sura.groovy

--- vars/sura.groovy
+++ vars/sura.groovy
@@ -1,19 +1,20 @@
 def call(url) {
-    gradle 'Gradle 2.3'
     pipeline {
-      agent any
-      stages {
-       stage('GIT') {
-            steps {
-                git credentialsId: '0df8e168-c5ab-452f-b993-29496cfbcd29', url: "${url}"
-
+        agent any
+        tools {
+            gradle 'Gradle 2.3'
+        }
+        stages {
+            stage('GIT') {
+                steps {
+                    git credentialsId: '0df8e168-c5ab-452f-b993-29496cfbcd29', url:'http://inboggit01.suramericana.com.co:8080/gitbucket/git/Devops/DevopsApi.git'
+                }
+            }
+            stage('Gradle') {
+                steps {
+                    sh 'gradle clean build'
+                }
             }
         }
-        stage('Gradle') {
-            steps {
-                sh 'gradle clean build'
-            }
-        }
-      }
-    }
+    } 
 } 
